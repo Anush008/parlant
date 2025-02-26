f96ada5fe5b2f5270e088a252ef72efad4e53259
Dor Zohar <dor@emcie.co>
2025-02-26 11:36:49 +0000

    Add tag association document db to glossary
    Signed-off-by: Dor Zohar <dor@emcie.co>

--- src/parlant/bin/server.py
+++ src/parlant/bin/server.py
@@ -345,6 +345,9 @@ async def initialize_container(
     services_db = await EXIT_STACK.enter_async_context(
         JSONFileDocumentDatabase(c[Logger], PARLANT_HOME_DIR / "services.json")
     )
+    glossary_tags_db = await EXIT_STACK.enter_async_context(
+        JSONFileDocumentDatabase(c[Logger], PARLANT_HOME_DIR / "glossary_tags.json")
+    )
 
     try:
         c[AgentStore] = await EXIT_STACK.enter_async_context(AgentDocumentStore(agents_db, migrate))
@@ -405,9 +408,10 @@ async def initialize_container(
         embedder_factory = EmbedderFactory(c)
         c[GlossaryStore] = await EXIT_STACK.enter_async_context(
             GlossaryVectorStore(
-                await EXIT_STACK.enter_async_context(
+                vector_db=await EXIT_STACK.enter_async_context(
                     ChromaDatabase(c[Logger], PARLANT_HOME_DIR, embedder_factory),
                 ),
+                document_db=glossary_tags_db,
                 embedder_type=type(await nlp_service.get_embedder()),
                 embedder_factory=embedder_factory,
             )
