44032da82a45fe5ba10a7c808b7f2942cae3992f
Menachem Brichta <menachem@emcie.co>
2025-03-10 17:04:11 +0000

    fix issue with clicking a log filter tab
    Signed-off-by: Menachem Brichta <menachem@emcie.co>

--- src/parlant/api/chat/src/components/message-fragment/message-fragment.tsx
+++ src/parlant/api/chat/src/components/message-fragment/message-fragment.tsx
@@ -21,7 +21,7 @@ const MessageFragment = ({fragment}: {fragment: {id: string; value: string}}) =>
 			<Tooltip value='Copy' side='top'>
 				<div
 					onClick={(e) => copy(fragment.id || '', e.currentTarget)}
-					className='hidden absolute right-[10px] top-[10px] cursor-pointer size-[28px] group-hover:flex justify-center items-center bg-white hover:bg-[#F3F5F9] border border-[#EEEEEE] hover:border-[#E9EBEF] rounded-[6px]'>
+					className='hidden absolute right-[10px] top-[8px] cursor-pointer size-[28px] group-hover:flex justify-center items-center bg-white hover:bg-[#F3F5F9] border border-[#EEEEEE] hover:border-[#E9EBEF] rounded-[6px]'>
 					<img src='icons/copy.svg' alt='' />
 				</div>
 			</Tooltip>
