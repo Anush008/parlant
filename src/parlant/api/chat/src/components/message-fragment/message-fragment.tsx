2a30e62ad1f1b9255e29d6ef3afdfde042ae757d
Menachem Brichta <menachem@emcie.co>
2025-03-10 15:08:30 +0000

    more style changes
    Signed-off-by: Menachem Brichta <menachem@emcie.co>

--- src/parlant/api/chat/src/components/message-fragment/message-fragment.tsx
+++ src/parlant/api/chat/src/components/message-fragment/message-fragment.tsx
@@ -14,7 +14,7 @@ import {twMerge} from 'tailwind-merge';
 const MessageFragment = ({fragment}: {fragment: {id: string; value: string}}) => {
 	return (
 		<div className='group relative flex justify-between group min-h-[40px] bg-white hover:bg-[#FAFAFA]'>
-			<div className='group [word-break:break-word] w-full flex gap-[17px] [&:first-child]:rounded-t-[3px] items-start text-[#656565] py-[8px] ps-[15px] pe-[38px]'>
+			<div className='group [word-break:break-word] w-full flex gap-[17px] font-light [&:first-child]:rounded-t-[3px] items-start text-[#656565] py-[8px] ps-[15px] pe-[38px]'>
 				<img src='icons/puzzle.svg' alt='' className='mt-[4px] w-[16px] min-w-[16px]' />
 				<div className={twMerge('invisible', fragment?.value && 'visible')}>{fragment?.value || 'loading'}</div>
 			</div>
